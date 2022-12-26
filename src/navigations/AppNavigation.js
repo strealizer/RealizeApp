824d2ab88d280487980c8fb1cb7fb8e419fdc247
agramitoki <strealizer@gmail.com>
2022-12-26 21:35:42 +0800

    test icons

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,16 +1,31 @@
 import * as React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 import HomeScreen from '../screens/Home/HomeScreen';
 import DetailsScreen from '../screens/Details/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
-function StackNavigator() {
+function ScreenWrapper() {
     return (
-        <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator 
+            screenOptions={{
+              headerTitleAlign: 'center'
+            }}>
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{
+              title: "Realize App",
+              headerStyle: {
+                backgroundColor: "#333"
+              },
+              headerTintColor: "#fff"
+            }} 
+          />
           <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
     );
@@ -19,7 +34,8 @@ function StackNavigator() {
 export default function AppContainer() {
     return (
       <NavigationContainer>
-        <StackNavigator></StackNavigator>
+        <StatusBar backgroundColor='#333' barStyle='light-content' />
+        <ScreenWrapper />
       </NavigationContainer>
     );
 }
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,17 +1,30 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function HomeScreen(props) {
 
   const { navigation } = props;
     
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
+    <View style={{ 
+      flex: 1, 
+      flexDirection: 'row',
+      alignItems: 'center', 
+      justifyContent: 'center',
+    }}>
+      <FontAwesome name="file-code-o" size={24} color="black" />
+      <Text style={{ 
+        fontSize: 18, 
+        fontWeight: 'bold',
+        paddingStart: 5 
+      }}>
+        ./src/screens/Home/HomeScreen.js
+      </Text>
+      {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 }
